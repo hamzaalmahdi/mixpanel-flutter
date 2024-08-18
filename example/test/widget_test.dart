58eb68332676d8c9f50a0fdbfdc551a393726ae7
Hamza Al-Mahdi <hmahdi@dinein.me>
2024-08-18 15:02:08 +0300

    Fix test issues

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mixpanel_example/main.dart';
+import 'package:mixpanel_flutter_example/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(FirstScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
